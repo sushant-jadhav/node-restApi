bcbc915943722f178673804d3437f64ac591a792
Sushant Jadhav <sushantjadhav2010@gmail.com>
2016-04-06 00:09:18 +0530

    User Router and save user in database

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var mongoose   = require('mongoose');
 var app = express();
 
 var Bear = require('./app/models/bear');
+var User = require('./app/models/user');
+//var users = require('./routes/user')(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 mongoose.connect('mongodb://localhost:27017/test');
 var port = process.env.PORT || 8080;
-
+var users = require('./routes/user')(app);
 var router = express.Router();
 // middleware to use for all requests
 router.use(function(req, res, next) {
@@ -104,6 +106,75 @@ router.route('/bears/:bear_id')
             res.json({ message: 'Successfully deleted' });
         });
     });
+
+router.route('/user')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+
+        var bear = new User();      // create a new instance of the Bear model
+        bear.name = req.body.name;  // set the bears name (comes from the request)
+
+        // save the bear and check for errors
+        bear.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'User created!' });
+        });
+
+    })
+    .get(function(req, res) {
+            User.find(function(err, bears) {
+                if (err)
+                    res.send(err);
+
+                res.json(bears);
+            });
+        });
+router.route('/user/:user_id')
+
+    // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+    .get(function(req, res) {
+        User.findById(req.params.bear_id, function(err, bear) {
+            if (err)
+                res.send(err);
+            res.json(bear);
+        });
+    })
+    // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
+    .put(function(req, res) {
+
+        // use our bear model to find the bear we want
+        User.findById(req.params.user_id, function(err, bear) {
+
+            if (err)
+                res.send(err);
+
+            bear.name = req.body.name;  // update the bears info
+
+            // save the bear
+            bear.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Bear updated!' });
+            });
+
+        });
+    })
+    // delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
+    .delete(function(req, res) {
+        User.remove({
+            _id: req.params.bear_id
+        }, function(err, bear) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
 
 app.use('/api', router);
 
